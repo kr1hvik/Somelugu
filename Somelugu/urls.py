2f4b6e28a9aaee088d2e0b82a285bc1e5fa9185a
Krihvik <partel.kukk@gmail.com>
2023-05-12 11:19:22 +0300

    doxc uritus

--- Somelugu/urls.py
+++ Somelugu/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('töölehed/', include('t77lehed.urls')),
+    path('', include('t77lehed.urls')),
 
 ]
 
--- t77lehed/views.py
+++ t77lehed/views.py
@@ -2,7 +2,35 @@ from django.shortcuts import render
 from .forms import ImageForm
 from django.http import HttpResponse
 from .models import Image
+from django.http import FileResponse
 import os
+
+
+
+""""
+f = open('foobar.docx', 'rb')
+document = Document(f)
+f.close()
+
+# or
+
+with open('foobar.docx', 'rb') as f:
+    source_stream = StringIO(f.read())
+document = Document(source_stream)
+source_stream.close()
+
+target_stream = StringIO()
+document.save(target_stream)
+"""
+
+def view_docx(request):
+    file_path = 'path/to/file.docx'
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    return HttpResponse("File not found")
 
 # Create your views here.
 
@@ -18,6 +46,6 @@ def gallery(request):
     context = {
             "form": form,
             "pilt":Image.objects.get(name="jome"),
-            "BASE_DIR":os.path.dirname(os.path.dirname(__file__))
+            "BASIC_DIR":os.getcwd(),
     }
     return render(request, "template/gallery.html", context)
