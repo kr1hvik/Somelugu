f84bde23e02f4a51c3f0d44ed428e35f611b8b32
Krihvik <partel.kukk@gmail.com>
2023-06-02 10:40:05 +0300

    content display

--- homepage/views.py
+++ homepage/views.py
@@ -4,11 +4,29 @@ from .forms import UploadFileForm
 from .models import Tekst
 from django.shortcuts import render
 from docx import Document
+from django.template.defaultfilters import linebreaksbr
 
 def home(response):
     return render(response, "homepage/main.html", {}) 
+
+
 def realica(request):
-    return render(request, "homepage/realica.html")
+    tekst_instance = Tekst.objects.get(id=10)
+    saved_file = tekst_instance.dokument
+    if saved_file.name.endswith('.docx'):
+        doc= Document(saved_file)
+        paragraphs= [paragraph.text for paragraph in doc.paragraphs]
+        return render(request, 'homepage/realica.html', {'sisu': paragraphs})
+    else:
+        with saved_file.open() as file:
+            file_content = file.read().decode("utf-8")
+            file_content = linebreaksbr(file_content)
+        return render(request, "homepage/realica.html", {"sisu":file_content})
+
+
+
+
+
 def upload_file(request):
     if request.method == 'POST':
         form= UploadFileForm(request.POST, request.FILES)
@@ -19,7 +37,7 @@ def upload_file(request):
         form= UploadFileForm()
     return render(request, 'homepage/upload.html', {'form': form})
 def vaatamine(request):
-    tekst_instance = Tekst.objects.get(id= 7)
+    tekst_instance = Tekst.objects.get(id=10)
     saved_file = tekst_instance.dokument
     if saved_file.name.endswith('.docx'):
          doc= Document(saved_file)
