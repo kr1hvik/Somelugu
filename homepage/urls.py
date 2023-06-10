5a4725976cbf7436b5ac661cd002b057e7e22d6c
Andkur21 <131437546+Andkur21@users.noreply.github.com>
2023-06-10 20:08:37 +0300

    vb rikub kõik ara

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,5 +6,5 @@ urlpatterns= [
     #re_path(r"^bio/(?P<username>\w+)/$", views.bio, name="bio"),
     path("", views.home, name= "home"),
     re_path('(?P<leht>[^/]+\.[^/]+)/$', views.realica, name='realica'),
-    path("vaata/", views.vaatamine,name= "vaatamine" ),
+    path('vaatamine/<int:tekst_id>/', views.vaatamine, name='vaatamine')
 ]
