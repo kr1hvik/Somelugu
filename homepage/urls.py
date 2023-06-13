261bf86598c7b041b26bde70dbe5cd9ce420ae03
Andkur21 <131437546+Andkur21@users.noreply.github.com>
2023-06-13 10:47:41 +0300

    dino

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,4 +6,5 @@ urlpatterns= [
     #re_path(r"^bio/(?P<username>\w+)/$", views.bio, name="bio"),
     path("", views.home, name= "home"),
     path('vaatamine/<str:title>/', views.vaatamine, name='vaatamine'),
+    path("dino", views.dino, name="dino")
 ]
